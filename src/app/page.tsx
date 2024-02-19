499b75ea11c930b556e065065fee35637826db4a
Noah Pittman <noahpittman00@gmail.com>
2024-02-19 11:38:35 -0500

    Update layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,15 @@ export default function Home() {
 
 	return (
 		<>
-			<div className="flex relative pt-2 w-full justify-center z-[99999]">
+			<div className="fixed end-0 p-2 z-[99999999999] flex items-center">
+				<Button variant={"link"}>
+					<a href="https://npitt.dev" target="_blank" rel="noopener noreferrer">
+						made with ❤️ by Noah Pittman
+					</a>
+				</Button>
+			</div>
+
+			<div className="flex relative pt-2 w-full justify-center z-10">
 				<Button onClick={handleAddBall}>Add Ball</Button>
 			</div>
 			<div className="fixed inset-0  mx-auto">
